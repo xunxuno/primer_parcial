5cb1de5166b0178525a7db7ab04f8558e8d8adc9
xunxuno <sintraxlch@gmail.com>
2024-02-29 13:11:00 -0600

    estilos globales

---
+++ proyecto/pagina.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Crear un nuevo div
+    var nuevoDiv = document.createElement('div');
+
+    // Configurar atributos o contenido (opcional)
+    nuevoDiv.id = 'menu-principal';
+    nuevoDiv.textContent = 'menu';
+
+    // Agregar el nuevo div al cuerpo del documento
+    document.body.appendChild(nuevoDiv);
+});
