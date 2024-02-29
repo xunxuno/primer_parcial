4c724e9b67df3cb4abf3afda566cd1592e5c2206
xunxuno <sintraxlch@gmail.com>
2024-02-29 15:55:29 -0600

    alineacion 2

--- proyecto/pagina.js
+++ proyecto/pagina.js
@@ -10,6 +10,8 @@ document.addEventListener('DOMContentLoaded', function() {
     nuevoDiv.style.backgroundColor = 'red';
     nuevoDiv.textContent = 'menu';
     nuevoDiv.style.display = 'flex';
+    nuevoDiv.style.justifyContent = 'space-between';
+    nuevoDiv.style.alignItems = 'center';
 
     // Agregar el nuevo div al cuerpo del documento
     document.body.appendChild(nuevoDiv);
