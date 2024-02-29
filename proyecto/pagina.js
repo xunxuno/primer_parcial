d47c1dbe64ca420fb9b2fe2ff2104275185431b0
xunxuno <sintraxlch@gmail.com>
2024-02-29 13:29:12 -0600

    color 4

--- proyecto/pagina.js
+++ proyecto/pagina.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // Configurar atributos o contenido (opcional)
     nuevoDiv.id = 'menu-principal';
     nuevoDiv.style.width = '1519px';
-    nuevoDiv.style.height = '71';
+    nuevoDiv.style.height = '703';
     nuevoDiv.style.backgroundColor = 'red';
     nuevoDiv.textContent = 'menu';
 
