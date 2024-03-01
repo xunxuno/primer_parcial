9fd808c80dba64932dea9b61f418ba026166bea3
xunxuno <sintraxlch@gmail.com>
2024-03-01 11:16:33 -0600

    correccion

--- proyecto/pagina.js
+++ proyecto/pagina.js
@@ -22,6 +22,7 @@ document.addEventListener('DOMContentLoaded', function() {
     //Estilos del nav
     nav.id = 'nav-principal';
     nav.style.height = '71px';
+    nav.style.width = '100%';
     nav.style.marginTop = '0%';
     nav.style.marginLeft = '140px';
     nav.style.marginRight = '140px';
