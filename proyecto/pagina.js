8b99122cafb8c4f8cc64b2b72cb7c97295fdba60
xunxuno <sintraxlch@gmail.com>
2024-03-01 14:37:46 -0600

    boton 1

--- proyecto/pagina.js
+++ proyecto/pagina.js
@@ -55,6 +55,7 @@ document.addEventListener('DOMContentLoaded', function() {
      boton1.style.border = '0';
      boton1.style.marginTop = '11px';
      boton1.style.marginBottom = '17px';
+     boton1.style.marginLeft = '0%';
  
 
  
