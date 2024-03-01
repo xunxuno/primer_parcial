d9fb699559ede11773cc6348c3102bbcf038e71e
xunxuno <sintraxlch@gmail.com>
2024-03-01 17:31:45 -0600

    margin red

--- proyecto/pagina.js
+++ proyecto/pagina.js
@@ -58,7 +58,7 @@ document.addEventListener('DOMContentLoaded', function() {
      boton1.style.paddingRight = '30px';
      boton1.style.backgroundColor = 'red';
 
-     //boton1.style.marginTop = '11px';
+     boton1.style.marginTop = '11px';
      boton1.style.marginBottom = '17px';
      boton1.style.marginLeft = '0%';
      boton1.style.display = 'flex';
