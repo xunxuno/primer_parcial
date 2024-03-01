ffefd7d356414bdc615f5c474692485e7b08f7ac
xunxuno <sintraxlch@gmail.com>
2024-03-01 11:45:16 -0600

    cambios

--- proyecto/pagina.js
+++ proyecto/pagina.js
@@ -29,33 +29,7 @@ document.addEventListener('DOMContentLoaded', function() {
     nav.style.alignItems = 'center';
     nav.style.display = 'flex';
 
-    // Crea elementos de lista (ul y li)
-    var ul = document.createElement('ul');
-    var li1 = document.createElement('li');
-    var li2 = document.createElement('li');
-
-    // Crea elementos de enlace (a)
-    var enlace1 = document.createElement('a');
-    var enlace2 = document.createElement('a');
-
-    // Agrega texto a los enlaces
-    enlace1.textContent = 'Inicio';
-    enlace2.textContent = 'Servicios';
-
-    // Establece atributos y estilos según sea necesario
-    enlace1.href = '#inicio';
-    enlace2.href = '#servicios';
-
-    // Agrega los enlaces a los elementos de la lista
-    li1.appendChild(enlace1);
-    li2.appendChild(enlace2);
-
-    // Agrega los elementos de la lista a la lista
-    ul.appendChild(li1);
-    ul.appendChild(li2);
-
-    // Agrega la lista al elemento de navegación
-    nav.appendChild(ul);
+    
 
     // Agrega el elemento de navegación al nuevo div
     nuevoDiv.appendChild(nav);
