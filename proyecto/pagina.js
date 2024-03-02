f54eb8f34ff968ed1175a71a217265548f782ddc
xunxuno <sintraxlch@gmail.com>
2024-03-01 21:43:49 -0600

    margin

--- proyecto/pagina.js
+++ proyecto/pagina.js
@@ -23,7 +23,7 @@ document.addEventListener('DOMContentLoaded', function() {
     nav.style.height = '71px';
     nav.style.width = '100%';
     nav.style.marginTop = '0%';
-    nav.style.marginLeft = '266px';
+    nav.style.marginLeft = '226px';
     nav.style.marginRight = '266px';
     nav.style.backgroundColor = 'blue';
     nav.style.display = 'flex';
