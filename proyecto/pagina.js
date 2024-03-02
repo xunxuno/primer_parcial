e56c4b4b03a672de82dbd961b25b8c251a5f0350
xunxuno <sintraxlch@gmail.com>
2024-03-01 22:05:45 -0600

    fixed

--- proyecto/pagina.js
+++ proyecto/pagina.js
@@ -10,6 +10,8 @@ document.addEventListener('DOMContentLoaded', function() {
     nuevoDiv.style.paddingTop = '0';
     nuevoDiv.style.backgroundColor = 'red';
     nuevoDiv.style.display = 'flex';
+    nuevoDiv.style.border= 'none';
+    nuevoDiv.style.position = 'fixed';
 
     //nuevoDiv.style.alignItems = 'center';
 
