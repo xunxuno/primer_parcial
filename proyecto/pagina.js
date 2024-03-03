2d3be5de899a84e737525016d2a4a90f1d895ee5
xunxuno <sintraxlch@gmail.com>
2024-03-03 12:54:30 -0600

    margin

--- proyecto/pagina.js
+++ proyecto/pagina.js
@@ -90,7 +90,7 @@ document.addEventListener('DOMContentLoaded', function() {
     boton2.style.width = '200px';
     boton2.style.textDecoration = 'holaaa';
     boton2.style.height = '50px';
-    boton2.style.marginRight = '30px';
+    boton2.style.marginRight = '60px';
  
 
  
