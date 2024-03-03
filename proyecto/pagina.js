4503ee6818c8d9dfd48da9fb9c4cea74025a9f7c
xunxuno <sintraxlch@gmail.com>
2024-03-03 14:58:38 -0600

    60

--- proyecto/pagina.js
+++ proyecto/pagina.js
@@ -93,7 +93,7 @@ document.addEventListener('DOMContentLoaded', function() {
     boton2.style.width = '240px';
     boton2.textContent = 'IDIOMA DE LA PÁGINA: ESPAÑOL';
     boton2.style.alignItems = 'center';
-    boton2.style.height = '50px';
+    boton2.style.height = '70px';
     boton2.style.backgroundColor = "transparent";
     boton2.style.marginTop = '10px';
     boton2.style.color = '#afafaf';
