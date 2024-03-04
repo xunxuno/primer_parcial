8fb83fdc0335a4cc5b504ca253bf439d8ef79469
xunxuno <sintraxlch@gmail.com>
2024-03-03 19:10:43 -0600

    80s

--- proyecto/pagina.js
+++ proyecto/pagina.js
@@ -131,7 +131,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // Imagen de menu de idiomas
     var btnMenu = document.createElement("img");
     btnMenu.src = 'src/idioma.svg';
-    btnMenu.style.marginLeft = '100px';
+    btnMenu.style.marginLeft = '80px';
 
     // Agregar imagen de menu a boton2
     boton2.appendChild(btnMenu);
