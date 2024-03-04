1ca2ee7842ea5e8e534ea4cc4fdbcdab94b94a60
xunxuno <sintraxlch@gmail.com>
2024-03-04 10:36:53 -0600

    margin botton

--- proyecto/pagina.js
+++ proyecto/pagina.js
@@ -89,7 +89,8 @@ document.addEventListener('DOMContentLoaded', function() {
     boton2.style.alignItems = 'center';
     boton2.style.height = '19px'; 
     boton2.style.backgroundColor = "transparent"; 
-    boton2.style.marginTop = '20px';
+   // boton2.style.marginTop = '20px';
+    boton2.style.marginBottom = '10px';
     boton2.style.color = '#afafaf';
     boton2.style.fontSize = '13px';
     boton2.style.lineHeight = '5px';
