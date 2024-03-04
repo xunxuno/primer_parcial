c691965af048058cb7b0f476f8dc746a89994a02
xunxuno <sintraxlch@gmail.com>
2024-03-04 11:53:45 -0600

    redo

--- proyecto/pagina.js
+++ proyecto/pagina.js
@@ -174,7 +174,7 @@ document.addEventListener('DOMContentLoaded', function() {
     //estilos tabla
     tabla.style.backgroundColor = 'blue';
     tabla.style.marginLeft = '30px';
-    tabla.style.marginTop = '20%';
+
 
 
     //agregar tabla al menu
