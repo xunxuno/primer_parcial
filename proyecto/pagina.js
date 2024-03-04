8dd185bcf91f551455979093a7d28da37aea9800
xunxuno <sintraxlch@gmail.com>
2024-03-04 00:41:08 -0600

    6

--- proyecto/pagina.js
+++ proyecto/pagina.js
@@ -92,7 +92,7 @@ document.addEventListener('DOMContentLoaded', function() {
     boton2.style.marginTop = '23px';
     boton2.style.color = '#afafaf';
     boton2.style.fontSize = '13px';
-    boton2.style.lineHeight = '5px';
+    boton2.style.lineHeight = '6px';
     boton2.style.letterSpacing = '0.8px';
     boton2.style.marginLeft = '0';
     boton2.style.textAlign = 'left';
