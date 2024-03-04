44b812f7717dda81fd6b7b4cdf17cc5a71f5b48f
xunxuno <sintraxlch@gmail.com>
2024-03-04 11:58:12 -0600

    6

--- proyecto/pagina.js
+++ proyecto/pagina.js
@@ -174,7 +174,7 @@ document.addEventListener('DOMContentLoaded', function() {
     //estilos tabla
     tabla.style.backgroundColor = 'blue';
     tabla.style.marginLeft = '30px';
-    tabla.style.paddingTop = '9%';
+    tabla.style.paddingTop = '6%';
 
 
 
