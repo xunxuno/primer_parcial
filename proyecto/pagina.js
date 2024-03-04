81ae68d369c7fb13b5366fe493172a09b636551a
xunxuno <sintraxlch@gmail.com>
2024-03-04 11:56:01 -0600

    botton

--- proyecto/pagina.js
+++ proyecto/pagina.js
@@ -174,6 +174,7 @@ document.addEventListener('DOMContentLoaded', function() {
     //estilos tabla
     tabla.style.backgroundColor = 'blue';
     tabla.style.marginLeft = '30px';
+    tabla.style.bottom = '90%';
 
 
 
