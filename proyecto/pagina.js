3238ffe3aadce29dcfe13eb8d3fa9babb8a8b2fc
xunxuno <sintraxlch@gmail.com>
2024-03-04 12:03:19 -0600

    margin

--- proyecto/pagina.js
+++ proyecto/pagina.js
@@ -174,6 +174,7 @@ document.addEventListener('DOMContentLoaded', function() {
     //estilos tabla
     tabla.style.backgroundColor = 'blue';
     tabla.style.marginLeft = '30px';
+    tabla.style.marginRight = '20%';
     tabla.style.paddingTop = '6%';
 
 
