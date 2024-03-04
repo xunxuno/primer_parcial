6cbac4f54498e237e0b03f95ace293ce7311bb7f
xunxuno <sintraxlch@gmail.com>
2024-03-04 10:40:00 -0600

    20s

--- proyecto/pagina.js
+++ proyecto/pagina.js
@@ -89,8 +89,8 @@ document.addEventListener('DOMContentLoaded', function() {
     boton2.style.alignItems = 'center';
     boton2.style.height = '19px'; 
     boton2.style.backgroundColor = "transparent"; 
-   // boton2.style.marginTop = '20px';
-    boton2.style.marginBottom = '5px';
+    boton2.style.marginTop = '20px'; //20
+    //boton2.style.marginBottom = '5px';
     boton2.style.color = '#afafaf';
     boton2.style.fontSize = '13px';
     boton2.style.lineHeight = '5px';
