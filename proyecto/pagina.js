4768d9e8a440b0a6b9d349d5335955e4d08e93ed
xunxuno <sintraxlch@gmail.com>
2024-03-05 13:50:20 -0600

    ddddd

--- proyecto/pagina.js
+++ proyecto/pagina.js
@@ -357,7 +357,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     var text2s2 = document.createElement('div');
     text1s2.appendChild(text2s2);
-    text2s2.style.width = '600px';
+    text2s2.style.width = '580px';
     text2s2.style.height = '200px';
     text2s2.textContent = 'Aprender con Duolingo es divertido y los estudios demuestran que funciona. ¡En nuestras lecciones cortas ganarás puntos y habilitarás nuevas unidades al mismo tiempo que desarrollas tus habilidades de comunicación en la vida real!';
     text2s2.style.fontSize = '17px';
