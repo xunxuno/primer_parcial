4e382f7b6dbc31abada98ea4fc9e11e6967c8b8a
xunxuno <sintraxlch@gmail.com>
2024-03-05 09:38:47 -0600

    cc

--- proyecto/pagina.js
+++ proyecto/pagina.js
@@ -248,6 +248,7 @@ document.addEventListener('DOMContentLoaded', function() {
     boton1seccion1borde.style.borderRadius = '12px';
     boton1seccion1borde.style.backgroundColor = 'transparent';
     boton1seccion1.style.borderColor = '#61B800';
+    boton1seccion1borde.style.border = '5px';
 
 
     // Añadir el nodo de texto al div
