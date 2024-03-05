9e3eb086ac03e738d66ded8ab65e910d2750cac9
xunxuno <sintraxlch@gmail.com>
2024-03-05 08:26:43 -0600

    33.6

--- proyecto/pagina.js
+++ proyecto/pagina.js
@@ -206,7 +206,7 @@ document.addEventListener('DOMContentLoaded', function() {
     texto1.style.fontSize = '31px';
     texto1.style.position = 'fixed';
     texto1.style.right = '20.4%'; //20.4
-    texto1.style.top = '33.5%';
+    texto1.style.top = '33.6%';
 
 
     // Añadir el nodo de texto al div
