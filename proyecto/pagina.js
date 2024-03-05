1a11d61e815bd5fa64ade471d704d778a5f928c1
xunxuno <sintraxlch@gmail.com>
2024-03-05 12:41:42 -0600

    feqf

--- proyecto/pagina.js
+++ proyecto/pagina.js
@@ -350,7 +350,8 @@ document.addEventListener('DOMContentLoaded', function() {
     text1s2.style.backgroundColor = 'yelllow';
     text1s2.style.width = '200px';
     text1s2.style.height = '100px';
-    text1s2.textContent = 'holaaa';
+    text1s2.textContent = 'divertido, efectivo y gratis';
+    text1s2.style.color = 'white';
     text1s2.style.fontSize = '70px';
     
     
