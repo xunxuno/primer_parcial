da2376588d344648d04ce366f0d70e0e32de36ed
xunxuno <sintraxlch@gmail.com>
2024-03-05 15:28:45 -0600

    tops

--- proyecto/pagina.js
+++ proyecto/pagina.js
@@ -549,6 +549,7 @@ document.addEventListener('DOMContentLoaded', function() {
     textocolor.style.fontSize = '70px';
     textocolor.style.fontWeight = "bolder";
     textocolor.style.color = "#042C60";
+    textocolor.style.marginTop = '20px';
 
 
 
