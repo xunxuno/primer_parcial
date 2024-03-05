b4ce4201970913ae0589e18840af0536b602d70f
xunxuno <sintraxlch@gmail.com>
2024-03-05 00:21:19 -0600

    20

--- proyecto/pagina.js
+++ proyecto/pagina.js
@@ -205,7 +205,7 @@ document.addEventListener('DOMContentLoaded', function() {
     texto1.textContent='¡La forma divertida, efectiva y';
     texto1.style.fontSize = '30px';
     texto1.style.position = 'fixed';
-    texto1.style.right = '25%';
+    texto1.style.right = '20%';
 
 
     // Añadir el nodo de texto al div
