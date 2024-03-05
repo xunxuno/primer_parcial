3063fbfc31a426cc64389c5cdaacd05cab8fdcbc
xunxuno <sintraxlch@gmail.com>
2024-03-05 15:52:48 -0600

    d

--- proyecto/pagina.js
+++ proyecto/pagina.js
@@ -564,7 +564,8 @@ document.addEventListener('DOMContentLoaded', function() {
     var imagen = document.createElement('img');
     imagen.style.height = '570px';
     imagen.style.width = 'auto';
-    imagen.style.marginLeft = '15%';
+    imagen.style.marginLeft = '5%';
+    imagen.style.marginTop = '5%';
 
     // Establecer la ruta de la imagen
     imagen.src = 'src/imagensuper.png';
