2376b4e89084123247b781143d14044ff14c1253
xunxuno <sintraxlch@gmail.com>
2024-03-05 10:03:03 -0600

    color

--- proyecto/pagina.js
+++ proyecto/pagina.js
@@ -262,7 +262,7 @@ document.addEventListener('DOMContentLoaded', function() {
     boton2seccion1.style.fontWeight = "bolder";
     boton2seccion1.style.color = '#199EDD';
     boton2seccion1.style.backgroundColor = '#FFFFFF';
-    boton2seccion1.style.border = 'none';
+    boton2seccion1.style.borderColor = '#CECECE';
     //borde de bt2
     var boton2seccion1borde = document.createElement('button');
     boton2seccion1borde.appendChild(boton2seccion1);
