bbb3109d59b93d808c36a7e42719f94495ee2730
xunxuno <sintraxlch@gmail.com>
2024-03-05 14:54:47 -0600

    dwd

--- proyecto/pagina.js
+++ proyecto/pagina.js
@@ -475,8 +475,7 @@ document.addEventListener('DOMContentLoaded', function() {
     imgtexto2_3.style.height = 'auto';
     imgtexto2_3.style.float = 'right';
     imgtexto2_3.style.marginTop = '-50px';
-    imgtexto2_3.style.marginRight = '20px';
-    imgtexto2_3.style.marginLeft = '10%';
+    imgtexto2_3.style.marginRight = '-20px';
 
     divtexto3s2.appendChild(imgtexto2_3);
 
