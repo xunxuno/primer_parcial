9b59d8293760e3828f9260ecf1f36fe38add0468
xunxuno <sintraxlch@gmail.com>
2024-03-05 11:09:10 -0600

    d

--- proyecto/pagina.js
+++ proyecto/pagina.js
@@ -195,7 +195,7 @@ document.addEventListener('DOMContentLoaded', function() {
     divPrimerTexto.style.backgroundColor = 'yellow';
     divPrimerTexto.style.width = '950px'; //850
     divPrimerTexto.style.height = '400px';
-    divPrimerTexto.style.marginLeft = '21%'; //24
+    divPrimerTexto.style.marginLeft = '20.5%'; //24
     divPrimerTexto.style.marginTop = '11.3%';
 
     var texto1 = document.createElement('div');
