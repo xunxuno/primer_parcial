ea808046faa28d2165e69b8c31e613df58f825a6
xunxuno <sintraxlch@gmail.com>
2024-03-05 10:38:26 -0600

    ajustes

--- proyecto/pagina.js
+++ proyecto/pagina.js
@@ -295,8 +295,8 @@ document.addEventListener('DOMContentLoaded', function() {
     //imagen primer texto
     var imgtexto1 = new Image();
     imgtexto1.src= 'src/imgtexto1.svg';
-    imgtexto1.style.width = '330px';
-    imgtexto1.style.height = '400px';
+    imgtexto1.style.width = '350px';
+    imgtexto1.style.height = '500px';
     //añadir imagen
     divPrimerTexto.appendChild(imgtexto1);
 
