e0bd92edf545fc479580116a02969e74e8dd2efc
xunxuno <sintraxlch@gmail.com>
2024-03-05 08:22:58 -0600

    fontsize

--- proyecto/pagina.js
+++ proyecto/pagina.js
@@ -203,7 +203,7 @@ document.addEventListener('DOMContentLoaded', function() {
     texto1.style.width='415px';
     texto1.style.height='100px';
     texto1.textContent='¡La forma divertida, efectiva y';
-    texto1.style.fontSize = '35px';
+    texto1.style.fontSize = '30px';
     texto1.style.position = 'fixed';
     texto1.style.right = '20.4%'; //20.4
 
