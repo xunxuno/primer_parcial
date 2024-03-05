c6cf58f647a7d4d806370dc1e06371d0fbc217ad
xunxuno <sintraxlch@gmail.com>
2024-03-05 11:33:57 -0600

    border

--- proyecto/pagina.js
+++ proyecto/pagina.js
@@ -323,7 +323,7 @@ document.addEventListener('DOMContentLoaded', function() {
     nav2.style.padding = '0';
     nav2.style.paddingLeft = '0';
     nav2.style.paddingRight = '0';
-    nav2.style.borderColor = '#CECECE';
+    nav2.style.border = '#CECECE';
     nav2.style.border = '1px';
 
     // segunda seccion ////////////////////
